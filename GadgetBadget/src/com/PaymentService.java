efd09566412f1665c70f1d0f582d02be1bcc00bc
jananijayaweera <janani.jayaweera97@gmail.com>
2021-04-25 18:09:47 +0530

    addinsertPaymentDetailsmethod

--- GadgetBadget/src/com/PaymentService.java
+++ GadgetBadget/src/com/PaymentService.java
@@ -1,5 +1,34 @@
 package com;
 
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+//For JSON
+import com.google.gson.*;
+
+import model.Payment;
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document;
+
 public class PaymentService {
+	
+	Payment PayObj = new Payment();
+	
+	//method to insert a payment details
+			@POST
+			@Path("/")
+			@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+			@Produces(MediaType.TEXT_PLAIN)
+			public String insertPaymentDetails(@FormParam("app_Id") String app_Id, @FormParam("cardType") String cardType,
+					@FormParam("nameOnCard") String nameOnCard, @FormParam("cardno") String cardno,
+					@FormParam("phone") String phone, @FormParam("expdate") String expdate, @FormParam("amount") String amount,
+					@FormParam("status") String status) {
+					String output = PayObj.insertPaymentDetails(app_Id, cardType, nameOnCard, cardno, phone, expdate, amount,
+						status);
+				return output;
+			}
 
 }
