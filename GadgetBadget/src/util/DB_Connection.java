a6750639af2bf8b82b1b75767f037f01e0d09ba2
pramodC135 <pramodmadhushan4@gmail.com>
2021-04-25 14:28:00 +0530

    ADDdbcon

---
+++ GadgetBadget/src/util/DB_Connection.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB_Connection {
+	
+	public Connection connect() 
+	{
+		Connection con = null;
+		
+			try
+			{
+				// Provide the correct details: DBServer/DBName, username, password
+				
+				Class.forName("com.mysql.jdbc.Driver");
+				con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget","root","");
+				
+				//For testing
+				System.out.print("Successfully Connected");
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+			
+			return con;
+	}
+
+}
