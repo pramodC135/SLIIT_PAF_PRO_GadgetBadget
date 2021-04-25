7efee385da5489765fcde6f0aecd90629d007dbf
jananijayaweera <janani.jayaweera97@gmail.com>
2021-04-25 19:11:53 +0530

    updatePaymentServiceadd

--- GadgetBadget/src/com/PaymentService.java
+++ GadgetBadget/src/com/PaymentService.java
@@ -13,20 +13,56 @@ import org.jsoup.*;
 import org.jsoup.parser.*; 
 import org.jsoup.nodes.Document;
 
+@Path("/Payments")
 public class PaymentService {
 	
 	Payment PayObj = new Payment();
+	
+	//method to read a payment details
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_HTML) 
+	public String readPayment() 
+	 { 
+	 return PayObj.readPayment(); 
+	 }
 	
 	//method to insert a payment details
 			@POST
 			@Path("/")
 			@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 			@Produces(MediaType.TEXT_PLAIN)
-			public String insertPaymentDetails(@FormParam("app_code") String app_Id, @FormParam("cardType") String cardType,
+			public String insertPayment(@FormParam("app_Code") String app_Code, @FormParam("cardType") String cardType,
 					@FormParam("nameOnCard") String nameOnCard, @FormParam("cardno") String cardno,
-					@FormParam("phone") String phone, @FormParam("expdate") String expdate, @FormParam("amount") String amount,
-					@FormParam("status") String status) {
-					String output = PayObj.insertPayment(app_Id, cardType, nameOnCard, cardno, phone, expdate, amount,
+					@FormParam("phone") String phone, @FormParam("expdate") String expdate, @FormParam("amount") String amount) 
+			{
+					String output = PayObj.insertPayment(app_Code, cardType, nameOnCard, cardno, phone, expdate, amount);
+					return output;
+			}
+			
+			
+			//method to update the status details	
+			@PUT
+			@Path("/")
+			@Consumes(MediaType.APPLICATION_JSON)
+			@Produces(MediaType.TEXT_PLAIN)
+			public String updatePayment(String payData) {
+
+				//Convert the input string to a JSON object
+				JsonObject PaymentObj = new JsonParser().parse(payData).getAsJsonObject();
+
+				//Read the values from the JSON object
+				String paymentID = PaymentObj.get("paymentID").getAsString();
+				String app_Code = PaymentObj.get("app_Code").getAsString();
+				String cardType = PaymentObj.get("cardType").getAsString();
+				String nameOnCard = PaymentObj.get("nameOnCard").getAsString();
+				String cardno = PaymentObj.get("cardno").getAsString();
+				String phone = PaymentObj.get("phone").getAsString();
+				String expdate = PaymentObj.get("expdate").getAsString();
+				String amount = PaymentObj.get("amount").getAsString();
+				String status = PaymentObj.get("status").getAsString();
+
+				String output = PayObj.updatePayment(paymentID, app_Code, cardType, nameOnCard, cardno, phone, expdate, amount,
 						status);
 				return output;
 			}
--- GadgetBadget/src/model/Payment.java
+++ GadgetBadget/src/model/Payment.java
@@ -9,9 +9,57 @@ import util.DB_Connection;
 
 public class Payment {
 	
+	//Retrieve payment details 
+		public String readPayment() {
+			
+			String output = "";
+			try {
+				
+				DB_Connection obj_DB_Connection= new DB_Connection();
+				Connection con = obj_DB_Connection.connect();
+				
+				if (con == null) {
+					return "Error while connecting to the database for reading.";
+				}
+				// Prepare the html table to be displayed
+				output = "<table border=\"1\"><tr> <th>AppoCode</th> <th>CardType</th> <th>Name</th> <th>CardNo</th> <th>Phone</th ><th>Exp_date</th> <th>Amount</th> </tr>";
+				String query = "select * from payment";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+				// iterate through the rows in the result set
+				while (rs.next()) {
+					String paymentID = Integer.toString(rs.getInt("paymentID"));
+					String app_Code = rs.getString("app_Code");
+					String cardType = rs.getString("cardType");
+					String nameOnCard = rs.getString("nameOnCard");
+					String cardno = Integer.toString(rs.getInt("cardno"));
+					String phone = rs.getString("phone");
+					String expdate = rs.getString("expdate");
+					String amount = Double.toString(rs.getDouble("amount"));
+					
+					// Add into the html table
+					output += "<tr><td>" + app_Code + "</td>";
+					output += "<td>" + cardType + "</td>";
+					output += "<td>" + nameOnCard + "</td>";
+					output += "<td>" + cardno + "</td>";
+					output += "<td>" + phone + "</td>";
+					output += "<td>" + expdate + "</td>";
+					output += "<td>" + amount + "</td>";
+					
+				}
+				con.close();
+				// Complete the html table
+				output += "</table>";
+			} catch (Exception e) {
+				output = "Error while reading the card details.";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+		
+	
 	//Insert new payment details
-		public String insertPayment(String app_code, String ctype, String name, String cardno, String pho, String expdate,
-				String amount, String status) {
+		public String insertPayment(String app_code, String ctype, String name, String cardno, String pho, String expdate, String amount) {
 			String output = "";
 			try {
 				DB_Connection obj_DB_Connection= new DB_Connection();
@@ -21,8 +69,7 @@ public class Payment {
 					return "Error while connecting to the database for inserting.";
 				}
 				// create a prepared statement
-				String query = " insert into payment (`paymentID`,`app_Code`,`cardType`,`nameOnCard`,`cardno`,`phone`,`expdate`,`amount`)"
-						+ " values (?,?, ?, ?, ?, ?, ?, ?)";
+				String query = " insert into payment (`paymentID`,`app_Code`,`cardType`,`nameOnCard`,`cardno`,`phone`,`expdate`,`amount`)" + " values (?,?, ?, ?, ?, ?, ?, ?)";
 				PreparedStatement preparedStmt = con.prepareStatement(query);
 				// binding values
 				preparedStmt.setInt(1, 0);
@@ -41,6 +88,42 @@ public class Payment {
 				output = "Inserted successfully";
 			} catch (Exception e) {
 				output = "Error while inserting the card details.";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+		
+		
+		//Update the status of payments
+		public String updatePayment(String paymentID, String app_Code, String ctype, String name, String cardno, String pho,
+				String expdate, String amount, String status) {
+			String output = "";
+			try {
+				DB_Connection obj_DB_Connection= new DB_Connection();
+				Connection con = obj_DB_Connection.connect();
+				if (con == null) {
+					return "Error while connecting to the database for updating.";
+				}
+				// create a prepared statement
+				String query = "UPDATE paymentdetails SET app_Code=?,cardType=?,nameOnCard=?,cardno=?,phone=?,expdate=?,amount=?,status=? WHERE paymentID=?";
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+				// binding values
+				preparedStmt.setString(1, app_Code);
+				preparedStmt.setString(2, ctype);
+				preparedStmt.setString(3, name);
+				preparedStmt.setInt(4, Integer.parseInt(cardno));
+				preparedStmt.setString(5, pho);
+				preparedStmt.setString(6, expdate);
+				preparedStmt.setDouble(7, Double.parseDouble(amount));
+				preparedStmt.setString(8, status);
+				preparedStmt.setInt(9, Integer.parseInt(paymentID));
+
+				// execute the statement
+				preparedStmt.execute();
+				con.close();
+				output = "Updated successfully";
+			} catch (Exception e) {
+				output = "Error while updating the card details.";
 				System.err.println(e.getMessage());
 			}
 			return output;
