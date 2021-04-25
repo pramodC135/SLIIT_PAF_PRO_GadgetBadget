0f1619d7bcc85c0afa7d328ed4432b71e2b59508
sakuna1997 <subi222222@gmail.com>
2021-04-25 15:35:36 +0530

    addImports

--- GadgetBadget/src/com/ProjectService.java
+++ GadgetBadget/src/com/ProjectService.java
@@ -1,5 +1,16 @@
 package com;
 
-public class ProjectService {
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+//For JSON
+import com.google.gson.*; 
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
 
+
+public class ProjectService {
+	
 }
