2a9b1b38f563c7e2751dc3b394ad7dfa8c7e4dc4
MalithChanuka <it18213768@my.sliit.lk>
2021-04-25 20:28:00 +0530

    readOrdersadd

--- GadgetBadget/src/com/OrderService.java
+++ GadgetBadget/src/com/OrderService.java
@@ -1,5 +1,27 @@
 package com;
 
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+//For JSON
+import com.google.gson.*;
+
+
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document;
+
+@Path("/Orders")
 public class OrderService {
+	
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_HTML) 
+	public String readOrders() 
+	{ 
+		return orderObj.readOrders(); 
+	}
 
 }
